e2b4d495fc70218d7c25e02604554e0243931638
Es <fatih@itsmoneo.com>
2024-08-11 02:15:25 +0300

    created address, category, contact information, food, ingredient category, ingredient item, order, order item, restaurent, user models and user role enum

---
+++ src/main/java/com/fatichdev/online_food_ordering/model/User.java
@@ -0,0 +1,44 @@
+package com.fatichdev.online_food_ordering.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fatichdev.online_food_ordering.dto.RestaurantDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.lang.model.type.ArrayType;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    // @JsonIgnore used to ignore the field when serializing to JSON
+    // @OneToMany used to define a one-to-many relationship
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDto> favorites = new ArrayList<>();
+
+    // orphanRemoval means that if an Address is removed from the list, it should be deleted from the database
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+}
---
+++ src/test/java/com/fatichdev/online_food_ordering/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatichdev.online_food_ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
